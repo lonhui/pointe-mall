3add531a5fef5d8a90b576cf6114b69252507fe6
LONGHUI <longhui@yogrt.co>
2018-10-18 19:59:43 +0800

    懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/Home/index'
-import Terms from '@/pages/Terms/index'
-import Shop from '@/pages/Shop/index'
 
 Vue.use(Router)
 
@@ -11,17 +8,17 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/Home/index')
     },
     {
       path: '/Terms',
       name: 'Terms',
-      component: Terms
+      component: () => import('@/pages/Terms/index')
     },
     {
       path: '/Shop',
       name: 'Shop',
-      component: Shop
+      component: () => import('@/pages/Shop/index')
     }
   ]
 })
